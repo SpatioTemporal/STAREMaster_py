b688b0380c4069283b1a2f4331743a32b9cd5c6c
Michael Lee Rilee <mike@bayesics.com>
2022-10-21 19:39:52 +0000

    Forgot to update filename.

--- staremaster/products/__init__.py
+++ staremaster/products/__init__.py
@@ -11,4 +11,4 @@ from staremaster.products.amsr import AMSR
 from staremaster.products.imerg import IMERG
 from staremaster.products.satcorps import satCORPS
 from staremaster.products.modis_tilegrid import ModisTile
-from staremaster.products.goes import GOES_ABI_FIXED_GRID
+from staremaster.products.goes_abi_fixed_grid import GOES_ABI_FIXED_GRID
